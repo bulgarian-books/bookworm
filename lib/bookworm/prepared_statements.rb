c2716cf9188ee1ee5b7d2e6e1adeeb880db3cbc0
meddle <meddlesmerch@gmail.com>
2016-06-22 22:05:27 +0300

    New prepared statements.

--- lib/bookworm/prepared_statements.rb
+++ lib/bookworm/prepared_statements.rb
@@ -7,12 +7,25 @@ module Bookworm
 
       settings.connection.prepare(
         'insert_publisher',
-        'INSERT INTO publishers (name, code, page) VALUES ($1, $2, $3)'
+        'INSERT INTO publishers (name, code, state, page) ' \
+        'VALUES ($1, $2, $3, $4) RETURNING id'
       )
 
       settings.connection.prepare(
         'insert_publisher_alias',
         'INSERT INTO publisher_aliases (name, publisher_id) VALUES ($1, $2)'
+      )
+
+      settings.connection.prepare(
+        'insert_publisher_contacts',
+        'INSERT INTO publisher_contacts (name, publisher_id) VALUES ($1, $2)'
+      )
+
+      settings.connection.prepare(
+        'insert_publisher_addresses',
+        'INSERT INTO publisher_addresses ' \
+        '(town, main, phone, email, site, publisher_id) ' \
+        'VALUES ($1, $2, $3, $4, $5, $6)'
       )
 
       settings.connection.prepare(
